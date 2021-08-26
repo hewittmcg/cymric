c5ac3f4ac9e994994d8704a0de73cac6dd67dabc
Hewitt McGaughey <hewittmcgaughey@gmail.com>
2021-08-26 10:13:31 -0400

    add test blinky and gitignore

---
+++ main.c
@@ -0,0 +1,38 @@
+#include "stm32f4xx.h"
+#include "stm32f4xx_hal.h"
+#include "stm32f4xx_hal_gpio.h"
+#include "stm32f4xx_hal_rcc.h"
+
+// Initialize the GPIO pin corresponding to LED2
+static void prv_led_gpio_init(void) {
+	GPIO_InitTypeDef s_init_struct;
+	__GPIOA_CLK_ENABLE();
+	
+	// Configure LED
+	s_init_struct.Pin = GPIO_PIN_5;
+	s_init_struct.Mode = GPIO_MODE_OUTPUT_PP;
+	s_init_struct.Speed = GPIO_SPEED_FAST;
+	HAL_GPIO_Init(GPIOA, &s_init_struct);
+}
+
+// Handler for SysTick interrupts (allows delays to work)
+void SysTick_Handler(void) {
+	HAL_IncTick();
+}
+
+#define LED_ON() (HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET))
+#define LED_OFF() (HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET))
+
+int main(void) {
+	HAL_Init();
+	
+	prv_led_gpio_init();
+
+	// Blink LED2
+	while(1) {
+		LED_ON();
+		HAL_Delay(500);
+		LED_OFF();
+		HAL_Delay(500);
+	}
+}
