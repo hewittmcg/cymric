94f8149f5d07cbe398c4051913c251c97b67a138
Hewitt McGaughey <hewittmcgaughey@gmail.com>
2021-08-28 14:53:34 -0400

    fix PendSV_Handler to access PSP instead of MSP, fix a bug where initial stack vars were set up incorrectly in cymric_task_new. seems to switch between tasks OK now.

--- cymric.c
+++ cymric.c
@@ -62,6 +62,8 @@ void SysTick_Handler(void) {
 			
 			s_switch_info.cur_top_addr = &s_tcbs[s_switch_info.cur_task].top_addr;
 			s_switch_info.next_top_addr = &s_tcbs[s_switch_info.next_task].top_addr;
+			
+			s_switch_info.cur_task = s_switch_info.next_task; // for future use
 			// Context switch by setting PendSV to pending
 			SCB->ICSR |= 1 << SCB_ICSR_PENDSVSET_Pos;
 		}
@@ -70,37 +72,31 @@ void SysTick_Handler(void) {
 
 // Handler for context switches (TODO)
 __asm void PendSV_Handler(void) {
-	// Push R4-R11 onto the stack
-	PUSH {R11}
-	PUSH {R10}
-	PUSH {R9}
-	PUSH {R8}
-	PUSH {R7}
-	PUSH {R6}
-	PUSH {R5}
-	PUSH {R4}
+	//CPSID I
+	// Since this is an exception an as such occurs in handler mode,
+	// need to get the PSP into a register to access it.
+	MRS R2,PSP 
+	
+	// Push R4-R11 onto the process stack
+	STMFD R2!,{R4-R11} 
 	
 	// TODO MAKE SURE THIS WORKS OK!!!! AND TEST CONTEXT SWITCHING!!!!!
 	// Copy current top of stack into the TCB for the current task
 	// TODO: need to fix this by using a double pointer or something so it updates the s_tcbs address
 	LDR R4,=__cpp(&s_switch_info.cur_top_addr)
 	LDR R4,[R4] // dereference? idk, TODO make sure this works!!!
-	STR SP,[R4]
+	STR R2,[R4]
 	
 	// Set the stack pointer to the top of stack of the new task
 	LDR R4,=__cpp(&s_switch_info.next_top_addr)
 	LDR R4,[R4] // TODO make sure works
-	LDR SP,[R4] // Idk if these two can be done in one line
+	LDR R2,[R4] // Idk if these two can be done in one line
 	
 	// Pop R4-R11 from the stack
-	POP {R4}
-	POP {R5}
-	POP {R6}
-	POP {R7}
-	POP {R8}
-	POP {R9}
-	POP {R10}
-	POP {R11}
+	LDMFD R2!,{R4-R11}
+	
+	// Move R2 back to PSP
+	MSR PSP,R2
 	
    	// return from handler
 	NOP // padding, to remove a warning
@@ -173,17 +169,17 @@ bool cymric_task_new(CymricTaskFunction func, void *args) {
 	
 	// PSR
 	*addr = PSR_DEFAULT;
-	addr++;
+	addr--;
 	
 	// PC - address of function
 	*addr = (uint32_t)func;
 	
-	// R0 - address of args (located 6 indices above PC)
-	addr += 6;
+	// R0 - address of args (located 6 indices below PC)
+	addr -= 6;
 	*addr = (uint32_t)args;
 	
-	// Top of stack is 8 indices above R0 for the other 8 registers stored
-	s_tcbs[s_cur_alloc_id].top_addr = addr + 8;
+	// Top of stack is 8 indices below R0 for the other 8 registers stored
+	s_tcbs[s_cur_alloc_id].top_addr = addr - 8;
 	
 	s_cur_alloc_id++;
 	return true;
